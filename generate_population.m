3769a9ec1843a63a457cdb26edc99face54650b4
Jess <77901330+jjesss@users.noreply.github.com>
2025-03-24 17:01:23 +0000

    Update generate_population.m

--- generate_population.m
+++ generate_population.m
@@ -17,21 +17,3 @@ function population = generate_population(ori,population_size)
             end
         end
     
- %{
-    Generating initial population, in a different way:
-        for n = 1:population_size
-            chromosome = zeros(10, 3);
-            for i = 1:10
-                for j = 1:3
-                    % random number in [1,4]
-                    if(j == 1)
-                        chromosome(i,j) = randi([1,4]);
-                    else
-                    chromosome(i,j) = randi([0,9]);
-                    end
-                end
-            end
-            % add chromosome to the population
-            population(n,:) =  reshape(chromosome',1,[]);
-        end
-%}
